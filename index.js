a40a47286870ee654b0e8802a7e070a7ff1dd6f8
Raul Nazario <rnazariogonzales@gmail.com>
2023-09-08 16:16:44 -0400

    Added Style to Table and Form

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let myTable = document.getElementById('table1');
+myTable.border = "1";
+myTable.style.fontSize = "25px";
 
-myTable.style.border = "1px black";
+let tHeading = document.getElementById('tableHeading');
+tHeading.style.textAlign = "center";
+tHeading.style.backgroundColor = "rgb(198, 198, 236)";
+
+let thElements = document.getElementsByTagName('th');
+    for (let i = 0; i < thElements.length; i++){
+        thElements[i].style.padding = '5px';
+        thElements[i].style.textAlign = 'center';
+    }
+
+let tdElements = document.getElementsByTagName('td');
+    for (let i = 0; i < tdElements.length; i++){
+        tdElements[i].style.padding = '5px';
+    }
+
+let tData = document.getElementById('data');
+tData.style.fontSize = '18px';
+
+
+let myForm = document.getElementById('form1');
+myForm.style.fontSize = "20px";
